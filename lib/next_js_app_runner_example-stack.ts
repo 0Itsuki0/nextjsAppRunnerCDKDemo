312dd78d95baab271c9378679494dbe8be884ba6
000itsuki000 <itsuki.h@collabo-style.co.jp>
2024-04-04 12:25:16 +0900

    Deploy Next.Js App as Docker Container on AWS AppRunner using CDK

--- lib/next_js_app_runner_example-stack.ts
+++ lib/next_js_app_runner_example-stack.ts
@@ -1,16 +1,72 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecrAsset from 'aws-cdk-lib/aws-ecr-assets'
+import * as apprunner from 'aws-cdk-lib/aws-apprunner'
+import * as iam from 'aws-cdk-lib/aws-iam'
+
 
 export class NextJsAppRunnerExampleStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    private imageAsset: ecrAsset.DockerImageAsset
+    private app: apprunner.CfnService
+    private instanceRole: iam.Role
+    private accessRole: iam.Role
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'NextJsAppRunnerExampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        this.createAccessRole()
+        // this.buildRepository()
+        this.buildAsset()
+        this.buildAppRunnerService()
+
+    }
+
+
+    // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-apprunner-service-authenticationconfiguration.html#cfn-apprunner-service-authenticationconfiguration-accessrolearn
+    private createAccessRole() {
+        this.instanceRole = new iam.Role(this, 'AppRunnerInstanceRole', {
+            assumedBy: new iam.ServicePrincipal('tasks.apprunner.amazonaws.com'),
+        })
+        this.instanceRole.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY)
+
+        this.accessRole = new iam.Role(this, 'AppRunnerAccessRole', {
+            assumedBy: new iam.ServicePrincipal('build.apprunner.amazonaws.com'),
+        })
+        this.accessRole.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY)
+    }
+
+    private buildAsset() {
+        this.imageAsset = new ecrAsset.DockerImageAsset(
+            this,
+            "nextJsAppRunnerDemoImageAsset", {
+            directory: "nextjs-apprunner-demo"
+        })
+        this.imageAsset.repository.grantPull(this.accessRole)
+    }
+
+
+    private buildAppRunnerService() {
+        this.app = new apprunner.CfnService(this, "nextJsAppRunnerDemoAppRunner", {
+            // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-apprunner-service-sourceconfiguration.html#cfn-apprunner-service-sourceconfiguration-authenticationconfiguration
+            sourceConfiguration: {
+                imageRepository: {
+                    imageIdentifier: this.imageAsset.imageUri,
+                    imageRepositoryType: "ECR",
+                    imageConfiguration: {
+                        port: "8080"
+                    }
+                },
+                autoDeploymentsEnabled: true,
+                authenticationConfiguration: {
+                    accessRoleArn: this.accessRole.roleArn
+                }
+            },
+            instanceConfiguration: {
+                instanceRoleArn: this.instanceRole.roleArn
+            }
+        })
+        this.app.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY)
+    }
+
 }
